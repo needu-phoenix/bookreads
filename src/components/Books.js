57317f400e0fbed36beff7153bd6e92c6671edd8
Chinedu Igbodo <igbodochinedu@gmail.com>
2021-08-11 20:35:12 +0100

    Third commit

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,27 +3,30 @@ import React, {Component} from "react";
 class Books extends Component {
     render() {
         let {books} = this.props;
-        
-        let renderedBooks = books.map(book => {
-            return (
-                <li key={book.id}>
-                    <div className="book">
-                        <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
-                            <div className="book-shelf-changer">
-                                <select defaultValue={book.shelf} onChange={(event) => this.props.handleChange(book,event.target.value)}>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                </select>
+        let renderedBooks = [];
+
+        if(Array.isArray(renderedBooks) && renderedBooks.length > 1) {
+            renderedBooks = books.map(book => {
+                return (
+                    <li key={book.id}>
+                        <div className="book">
+                            <div className="book-top">
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}></div>
+                                <div className="book-shelf-changer">
+                                    <select defaultValue={book.shelf} onChange={(event) => this.props.handleChange(book,event.target.value)}>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </li>
-            )
-        })
+                    </li>
+                )
+            })
+        }
 
         return (
             <ol className="books-grid">
